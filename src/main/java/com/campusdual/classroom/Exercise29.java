45a5996a49f80165662bc7bef089bcaf54b9473a
oscardasilvarodriguez <grelodj@hotmail.com>
2025-02-11 00:07:58 +0100

    ejercicio-29-java resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,13 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
+        try {
+            throw new RuntimeException("Esta es una excepción controlada en el método main");
+        } catch (RuntimeException e) {
+            System.out.println("Se ha capturado una excepción: " + e.getMessage());
+        }
 
+        System.out.println("El programa ha finalizado de forma controlada");
     }
 }
+
